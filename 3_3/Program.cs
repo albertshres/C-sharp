cfd770a74c09ec6b25b3ca79fa17ce55ddceb876
Albert <albertshres2059@gmail.com>
2024-03-25 12:09:54 +0545

    added

---
+++ 3_3/Program.cs
@@ -0,0 +1,38 @@
+﻿//3 Write a program defining your custom exception
+using System;
+namespace myapp
+{
+    class MyException : ApplicationException { 
+    
+    public MyException() {
+            Console.WriteLine("An Exception");
+        }
+    public void DivideZeroException()
+        {
+            Console.WriteLine("Divide By Zero Exception");
+        }
+    
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+            int num, div, res;
+            Console.WriteLine("Enter a number :");
+            num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a divisor");
+            div = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                if (div == 0)
+                {
+                    throw new MyException();
+                }
+            }
+            catch(MyException e) { 
+            e.DivideZeroException();
+                    }
+
+        }
+    }
+}
