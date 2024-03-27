9db5cc2ca60b6ed45b5520d53549bc0bde0d9294
Albert <albertshres2059@gmail.com>
2024-03-27 13:44:28 +0545

    added

---
+++ 4_1/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _4_1.Controllers
+{
+    public class HelloWorldController : Controller
+    { 
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public string Name(string name,int roll) {
+            //localhost:7239/HelloWorld/Name?name=Ram&roll=2 write this in url to get the value i.e get request
+
+            string res =$"The name is {name} and has roll number {roll}";
+            return res;
+        }
+        public int Sum(int a,int b)
+        //localhost:7239/HelloWorld/Sum?a=2&b=3
+        {
+            return a + b;
+        }
+
+    }
+}
