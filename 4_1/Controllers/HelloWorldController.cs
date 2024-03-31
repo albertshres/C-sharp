310625d169834ee8165176b37d23b6ec7fe0a09a
Albert <albertshres2059@gmail.com>
2024-03-31 14:07:05 +0545

    added

--- 4_1/Controllers/HelloWorldController.cs
+++ 4_1/Controllers/HelloWorldController.cs
@@ -8,34 +8,59 @@ using Microsoft.AspNetCore.Mvc;
 namespace _4_1.Controllers
 {
     public class HelloWorldController : Controller
-    { 
-        
+    {
+        /*
+       public string Index()
+       {
+           return "Hello World";
+       }
+       */
         public IActionResult Index()
         {
             return View();//now goes to the views file
         }
-        
-        /*
-        public string Index()
-        {
-            return "Hello World";
-        }
-        */
-        public IActionResult Loop() { 
 
-        return View();
-            
+
+        public IActionResult Loop()
+        {
+
+            return View();
+
         }
-        public string Name(string name,int roll) {
+        /*
+        public string Name(string name, int roll)
+        {
             //localhost:7239/HelloWorld/Name?name=Ram&roll=2 write this in url to get the value i.e get request
 
-            string res =$"The name is {name} and has roll number {roll}";
+            string res = $"The name is {name} and has roll number {roll}";
             return res;
         }
-        public int Sum(int a,int b)
+        */
+
+        public IActionResult Name(string name,int roll)
+        {
+            ViewData["name"] = name;
+            ViewData["roll"] = roll;
+            return View();
+        }
+        /*
+        public int Sum(int a, int b)
         //localhost:7239/HelloWorld/Sum?a=2&b=3
         {
             return a + b;
+        }
+        */
+        public IActionResult Sum(int a, int b)
+        {
+            ViewData["first"] = a;
+            ViewData["second"] = b;
+            return View();
+        }
+
+        public IActionResult Switch_case(int rating)
+        {
+            ViewData["rating"] = rating;
+            return View();
         }
 
     }
